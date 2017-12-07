e0f9ee64e7f768d9a2d1cbbf89b1019e996e2a43
Connor Wilby <CJace33@gmail.com>
2017-12-07 21:26:06 +0000

    Re-added sprites and worked on grid generation

---
+++ Final Project/Assets/NodeScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeScript : MonoBehaviour {
+
+    public int NodeX;
+    public int NodeY;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
