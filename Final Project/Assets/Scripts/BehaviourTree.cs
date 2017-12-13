f2e0210aa4745c18dbfb4131cd688f6a58d3d65e
CJace33 <s176047@UCS.ac.uk>
2017-12-13 00:42:32 +0000

    Worked a little bit more on behaviour tree

--- Final Project/Assets/Scripts/BehaviourTree.cs
+++ Final Project/Assets/Scripts/BehaviourTree.cs
@@ -8,6 +8,8 @@ using UnityEngine;
 
 public class BehaviourTree : MonoBehaviour {
 
+    protected BTRoot BTRootObj = new BTRoot();
+
 	// Use this for initialization
 	void Start () {
 		
@@ -19,11 +21,37 @@ public class BehaviourTree : MonoBehaviour {
 	}
 }
 
-enum CompletionStates
+public enum CompletionStates
 {
     Success,
     Failure,
     Error
+}
+
+//Root class object of the tree
+public class BTRoot
+{
+
+    public CompletionStates Tick()
+    {
+        return CompletionStates.Error;
+    }
+
+}
+
+//Composites can have one or more children and run them in some order,
+//returning the value that is passed down to them from the child they are running
+public class Composite : BTRoot
+{
+
+}
+
+//Selectors attempt to tick its children sequentually until one of them returns.
+//When a child returns, it will attempt to run the next one until it runs out of children,
+//If all of its children 
+public class Selector : Composite
+{
+
 }
 
 
