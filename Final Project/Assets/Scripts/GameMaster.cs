caaf2866b43b22aab43ab7fc5d30913aa19f13a1
CJace33 <CJace33@gmail.com>
2018-02-19 16:26:06 +0000

    Found new tileset and reworked node grid again

---
+++ Final Project/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    //Create a node array of 20x20 nodes
+    public NodeRow[] nodeRow;
+    //public Node node;
+
+    // Use this for initialization
+    void Start ()
+    {
+        nodeRow = new NodeRow[10];
+        UpdateGrid();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    //Used to initially generate the grid
+    void GenerateGrid()
+    {
+
+    }
+    //Used to update the grid 
+    void UpdateGrid()
+    {
+        for (int row = 0; row < nodeRow.Length; row++)
+        {
+            for (int column = 0; column < nodeRow[row].columnData.Length; column++)
+            {
+                nodeRow[row].columnData[column].nodePos = new Vector3(row, column, 0);
+            }
+        }
+
+
+
+        //For each node that is a child of the gamemaster
+        foreach (NodeRow row in nodeRow)
+        {
+            foreach (Node node in row.columnData)
+            {
+
+            }
+        }
+    }
+
+}
+
+[System.Serializable]
+public class NodeRow
+{
+    public Node[] columnData = new Node[10];
+}
+
+
+[System.Serializable]
+public class Node
+{
+    //The Node's position in the world
+    public Vector3 nodePos;
+    //Bools to determine if there is a wall 
+    [Header("Passable")]
+    public bool north;
+    public bool east;
+    public bool south;
+    public bool west;
+    
+
+
+
+}
--- Final Project/Assets/Scripts/NodeScript.cs
+++ Final Project/Assets/Scripts/NodeScript.cs
@@ -6,41 +6,17 @@ public class NodeScript : MonoBehaviour {
 
     //Node's location on the grid
     public int nodeX, nodeY;
-    //If the node is passable or not
-    public bool passable;
-    //Enum Node Type
-    public enum TileType
-    {
-        Ground,
-        Wall
-    }
-    //Actual type associated with this
-    public TileType tiletype;
+    //How much this tile arbitrarily costs to move through
+    public float heuristic;
+
     GameObject thisObject;
     SpriteRenderer sr;
-    public Sprite[] sprites;
 
     void Awake()
     {
         thisObject = this.gameObject;
         //Set the sprite renderer
         sr = thisObject.GetComponent<SpriteRenderer>();
-
-        if (tiletype == TileType.Ground)
-        {
-            sr.sprite = sprites[0];
-            passable = true;
-        }
-        if (tiletype == TileType.Wall)
-        {
-            sr.sprite = sprites[1];
-            passable = false;
-        }
-
-        if (passable)
-        {
-            Passable();
-        }
     }
 
 	// Use this for initialization
@@ -60,7 +36,16 @@ public class NodeScript : MonoBehaviour {
 
         //Set gravity to 0
         rigidb.gravityScale = 0;
+    }
 
+    //Updates the walls 
+    public void UpdateSolids()
+    {
+
+    }
+    //Finds the neighboring nodes, should be called after UpdateSolids
+    public void UpdateNeighbors()
+    {
 
     }
 
